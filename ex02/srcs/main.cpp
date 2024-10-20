b281fde7e456af75b28dd82a4b34d77859b82f24
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-10-21 00:06:50 +0200

    fix: fix ex02 missing argument error message

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 21:59:17 by ibertran          #+#    #+#             */
-/*   Updated: 2024/10/20 05:18:38 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/10/21 00:06:35 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 int main(int ac, char **av) {
 	if (ac < 2) {
-		std::cout << "Usage: ..." << std::endl;
+		std::cout << "Error: no argument provided" << std::endl;
 		return (1);
 	}
 	{
